808f8c518d746458ce0cbaee6781cc0e716e1b09
Manh2k10 <dminhanh2810@gmail.com>
2024-03-28 20:55:26 +0700

    up

--- card.h
+++ card.h
@@ -1,12 +1,10 @@
-#ifndef _CARD_H_
-#define _CARD_H_
-
-#include <iostream>
+#pragma once
 #include <vector>
 #include <SDL.h>
 #include <SDL_image.h>
 #include "sdl_ultis.h"
 #include<string>
+using namespace std;
 enum COLOR {wild, red, green, blue, yellow};
 
 class card
@@ -14,6 +12,7 @@ class card
     public:
 		int number; // 0-9 numbers, +2, skip, reverse, all color, +4 (all color)
 		COLOR color; // 5 colors: red, green, blue, yellow, and no color
+        SDL_Texture* mycard;
 
 		bool operator==(card const & other) const;
 
@@ -21,14 +20,10 @@ class card
 
 		card();
 
-		card(int value, COLOR color, SDL_Texture* texture,SDL_Renderer*renderer);
+		card(int value, COLOR color,SDL_Texture* texture);
 
 
-		SDL_Texture* mycard;
-		SDL_Renderer*myrender;
+		static void createcard(vector <card> &allcard,SDL_Renderer* renderer);
 };
-vector <card>allcard;
-void createcard();
 
-#endif
 
