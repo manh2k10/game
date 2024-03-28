88c6064b66726e772c336c2461b8f221e2d9ddbb
Manh2k10 <dminhanh2810@gmail.com>
2024-03-28 20:56:00 +0700

    up

--- card.cpp
+++ card.cpp
@@ -1,16 +1,12 @@
 #include "card.h"
-#include <iostream>
-#include <vector>
-#include <SDL.h>
-#include <SDL_image.h>
-#include "sdl_ultis.h"
-#include<string>
-card::card() : number(0), color(wild),mycard(NULL),myrender(NULL)
+
+card::card() : number(0), color(wild)
 {
+
 
 }
 
-card::card(int num, COLOR col,SDL_Texture* texture,SDL_Renderer*renderer) : number(num), color(col), mycard(texture),myrender(renderer)
+card::card(int num, COLOR col,SDL_Texture* texture) : number(num), color(col),mycard(texture)
 {
 
 }
@@ -26,10 +22,11 @@ bool card::operator!=(card const & other) const
 {
 	return !(*this == other) ;
 }
-
-void createcard()
+card tmp;
+void card::createcard(vector <card> &allcard,SDL_Renderer* renderer)
 {
-    card tmp;
+   // vector <card> allcard;
+
     for(int i=wild;i<=yellow;i++)
     {
         string s;
@@ -38,68 +35,99 @@ void createcard()
         {
             tmp.number=13;
             tmp.color=wild;
-            tmp.mycard=loadtexture("images/wild13.png",tmp.myrender);
-            SDL_RenderCopy(tmp.myrender,tmp.mycard,NULL,NULL);
+            tmp.mycard=loadtexture("images/wild13.png",renderer);
+
             allcard.push_back(tmp);
             tmp.number=14;
             tmp.color=wild;
-            tmp.mycard=loadtexture("images/wild14.png",tmp.myrender);
-            SDL_RenderCopy(tmp.myrender,tmp.mycard,NULL,NULL);
+            tmp.mycard=loadtexture("images/wild14.png",renderer);
+
             allcard.push_back(tmp);
 
         }
         else for(int j=0;j<=12;j++)
         {
+
             switch(i)
             {
                  case red:
                      {
                           s="images/red.png";
-                          s.insert(10,1,char(j+48));
+                          if(j<10) s.insert(10,1,char(j+48));
+                          else
+                            switch(j)
+                          {
+                              case 10:s.insert(10,"10");break;
+                              case 11:s.insert(10,"11");break;
+                              case 12:s.insert(10,"12");break;
+                          }
                           tmp.number=j;
                           tmp.color=red;
-                          tmp.mycard=loadtexture(s,tmp.myrender);
-                          SDL_RenderCopy(tmp.myrender,tmp.mycard,NULL,NULL);
+                          tmp.mycard=loadtexture(s,renderer);
+
                           allcard.push_back(tmp);
+
                           break;
                      }
                 case blue:
                      {
+
                           s="images/blue.png";
-                          s.insert(11,1,char(j+48));
+                          if(j<10) s.insert(11,1,char(j+48));
+                          else
+                            switch(j)
+                          {
+                              case 10:s.insert(11,"10");break;
+                              case 11:s.insert(11,"11");break;
+                              case 12:s.insert(11,"12");break;
+                          }
                           tmp.number=j;
                           tmp.color=blue;
-                          tmp.mycard=loadtexture(s,tmp.myrender);
-                          SDL_RenderCopy(tmp.myrender,tmp.mycard,NULL,NULL);
+                          tmp.mycard=loadtexture(s,renderer);
                           allcard.push_back(tmp);
                           break;
                      }
                 case green:
                      {
                           s="images/green.png";
-                          s.insert(12,1,char(j+48));
+                          if(j<10) s.insert(12,1,char(j+48));
+                          else
+                            switch(j)
+                          {
+                              case 10:s.insert(12,"10");break;
+                              case 11:s.insert(12,"11");break;
+                              case 12:s.insert(12,"12");break;
+                          }
                           tmp.number=j;
                           tmp.color=green;
-                          tmp.mycard=loadtexture(s,tmp.myrender);
-                          SDL_RenderCopy(tmp.myrender,tmp.mycard,NULL,NULL);
+                           tmp.mycard=loadtexture(s,renderer);
+
                           allcard.push_back(tmp);
+
                           break;
                      }
                 case yellow:
                      {
                           s="images/yellow.png";
-                          s.insert(13,1,char(j+48));
+                          if(j<10) s.insert(13,1,char(j+48));
+                          else
+                            switch(j)
+                          {
+                              case 10:s.insert(13,"10");break;
+                              case 11:s.insert(13,"11");break;
+                              case 12:s.insert(13,"12");break;
+                          }
                           tmp.number=j;
                           tmp.color=yellow;
-                          tmp.mycard=loadtexture(s,tmp.myrender);
-                          SDL_RenderCopy(tmp.myrender,tmp.mycard,NULL,NULL);
+                          tmp.mycard=loadtexture(s,renderer);
+
+                          allcard.push_back(tmp);
                           break;
                      }
 
 
-
             }
-
+            // if(tmp.mycard==nullptr)cout<<j<<' '<<tmp.color<<endl;
 
         }
     }
