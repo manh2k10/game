3febb954f84cd6fe1631f30ca1d56c59c9c6e707
Manh2k10 <dminhanh2810@gmail.com>
2024-04-07 21:43:26 +0700

    7/4

--- deck.h
+++ deck.h
@@ -1,25 +1,27 @@
 #pragma once
 #include "card.h"
+#include <cstdlib>
+#include <ctime>
 #include "graphics.h"
-#include<vector>
+
 class deck : public card {
-	public:
-		deck();
-		deck(const deck & other);
-		const deck & operator= (const deck & other);
-		~deck();
-		void shuffle();
-		void create();
-		card draw();
-		int add_card(card temp_card);
-		void quick_shuffle();
-		int get_size();
+public:
+    deck();
+    deck(const deck &other);
+    const deck &operator= (const deck &other);
+    ~deck();
 
-	private:
-		vector<card> ptr_deck;
-		int size;
-		void clear();
-		void copy(const deck & other);
+    void shuffle();
+    void create();
+    card draw();
+    int add_card(card temp_card);
+    void quick_shuffle();
+    int get_size();
+
+private:
+    card* ptr_deck;
+    int size;
+
+    void clear();
+    void copy(const deck &other);
 };
-
-
