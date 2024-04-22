c5c79f43b2bb545f03c4204afc7af74efc1090c7
Manh2k10 <dminhanh2810@gmail.com>
2024-04-22 23:46:21 +0700

    22/4

--- def.h
+++ def.h
@@ -1,6 +1,14 @@
 #pragma once
 
-#define width 800
+#define width 900
 #define height 600
 #define title "uno!!"
+#include<SDL_image.h>
+#include<SDL.h>
+#define wcard 81
+#define hcard 126
+#define mxwid 800
+
+
+
 
